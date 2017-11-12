ac0bac8a838faa614941aee5a0c0ab01f1020c3a
DaniVSainz <danivsainz@gmail.com>
2017-11-12 11:42:48 -0500

    .

---
+++ playground/mongodb-find.js
@@ -0,0 +1,25 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+// var user = {name: 'Daniel', age:25};
+// var {name} = user;
+// console.log(name);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) => {
+    if (err) {
+       return console.log('Unable to connect to mongoDB server');
+    }
+    console.log('Connected to mongoDb server');
+
+    db.collection('Todos').find().toArray().then((docs) => {
+        console.log('Todos');
+        console.log(JSON.stringify(docs, undefined,2));
+    }, (err) => {
+        conosle.log('Unable to fetch documents', err);
+    });
+
+    db.close();
+});
