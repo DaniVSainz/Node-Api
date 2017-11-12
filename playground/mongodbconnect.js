d29e71c2476b4baea2573e3bdcd339efea71a553
DaniVSainz <danivsainz@gmail.com>
2017-11-12 01:01:16 -0500

    .

--- playground/mongodbconnect.js
+++ playground/mongodbconnect.js
@@ -1,8 +1,20 @@
 const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mmongodb://localhost:27017/TodoApp', (err,db) => {
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) => {
     if (err) {
-        console.log('Unable to connect to mongoDB server');
+       return console.log('Unable to connect to mongoDB server');
     }
     console.log('Connected to mongoDb server');
+
+    db.collection('Todos').insertOne({
+        text:'Something to do',
+        completed: false
+    }, (err,result)  => {
+        if (err){
+            return console.log('Unable to connect', err);
+        }
+        console.log(JSON.stringify(result.ops, undefined,2));
+    });
+
+    db.close();
 });
