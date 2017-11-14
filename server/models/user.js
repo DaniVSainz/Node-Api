5a56a91afbea1ba510df729f07fb589a02585d8e
DaniVSainz <danivsainz@gmail.com>
2017-11-13 19:48:52 -0500

    .

---
+++ server/models/user.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    password: {
+        type: String,
+        required:true,
+        minlength: 4,
+        trim: true
+    }
+})
+
+module.epxorts = {User}
