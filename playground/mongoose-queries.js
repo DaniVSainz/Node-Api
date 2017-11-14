ead0a52ceecade910bd3acf0782d29d308a63ef2
DaniVSainz <danivsainz@gmail.com>
2017-11-13 22:37:19 -0500

    .

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,4 +1,31 @@
-const {mongoose} = require('./../server/db.mongoose');
+const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
 
-var id = '5a0a4eb91206d7cb3d83512d';
+// var id = '5a0a60594546c3d5516d45f6';
+
+// Todo.find({
+//     _id: id
+// }).then((todos)=>{
+//     console.log('Todos',todos);
+// });
+
+// Todo.findOne({
+//     _id:id
+// }).then((todos) => {
+//     console.log('Todos',todos);
+// });
+
+// Todo.findById(id).then((todos) => {
+//     console.log('Todos',todos);
+// });
+
+User.findById('5a0a39b746dd0d4d1819564f').then((user) =>{
+    if(!user){
+        return console.log('Unable to find user');
+    }
+    console.log(JSON.stringify(user,undefined,2));
+}, (e) =>{
+    console.log(e);
+});
+
--- server/models/user.js
+++ server/models/user.js
@@ -16,4 +16,4 @@ var User = mongoose.model('User', {
     }
 })
 
-module.epxorts = {User}
+module.exports = {User}
