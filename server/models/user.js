454f80054aa9a0866fbece0e743e1751fa324c60
DaniVSainz <danivsainz@gmail.com>
2017-11-14 21:43:08 -0500

    .

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,34 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+
 
 var User = mongoose.model('User', {
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 1
+    minlength: 1,
+    validate: {
+      validator: (value) =>{
+        return validator.isEmail(value)
+      },
+      message: `${VALUE} : is not a valid email`
+    },
+    password: {
+      type: String,
+      require: true,
+      minlength: 6
+    },
+    tokens: [{
+      access: {
+        type: String,
+        require:true
+      },
+      token:{
+        type: String,
+        require: true
+      }
+    }]
   }
 });
 
