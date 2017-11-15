fd973baf729b2b4e204fcf9aabe4fc4d377e73f3
DaniVSainz <danivsainz@gmail.com>
2017-11-14 19:42:35 -0500

    .

--- server/server.js
+++ server/server.js
@@ -10,46 +10,46 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', function(req, res) {
-    var todo = new Todo({
-        text: req.body.text
-    });
-    
-    todo.save().then((doc) => {
-        res.send(doc);
-    }, (e) =>{
-        res.status(400).send(e);
-    });
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-app.get('/todos', (req,res) => {
-    Todo.find().then((todos) =>{
-        res.send({todos})
-    }, (e) =>{
-        res.status(400).send(e);
-    });
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-app.get('/todos/:id', (req,res) => {
-    var id = req.params.id
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
 
-    if(!ObjectID.isValid(id)){
-        console.log(`ID: ${id} is invalid`);        
-        return res.status(404).send();
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
     }
 
-    Todo.findById(id).then((todo) =>{
-        if (!todo) {
-            return res.status(404).send();
-        }
-        res.send({todo})
-    }).catch((e)=> {
-        res.status(400).send();
-    });
-})
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 app.listen(3000, () => {
-    console.log('App listening on port 3000!');
+  console.log('Started on port 3000');
 });
 
 module.exports = {app};
