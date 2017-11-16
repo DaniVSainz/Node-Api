37ef0189e478bb9fdf265cb31fedd1f2f527df45
DaniVSainz <danivsainz@gmail.com>
2017-11-15 19:38:07 -0500

    work inside of user model and server"

--- server/server.js
+++ server/server.js
@@ -104,8 +104,13 @@ app.post('/users', (req,res)=>{
   var body = _.pick(req.body, ['email','password']);
   var user = new User(body);
 
+  
+
   user.save().then((user) =>{
-    res.send(user);
+    return user.generateAuthToken();
+    // res.send(user);
+  }).then((token) =>{
+    res.header('x-auth', token).send(user);
   }).catch((e)=>{
     res.status(400).send(e);
   });
